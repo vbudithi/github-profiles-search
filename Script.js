1a31ddab2d199a0eadd7b8471a6c906174219284
vbudithi <vivek.budithi@gmail.com>
2020-10-31 02:02:30 +0930

    font awesome icons and styling

--- Script.js
+++ Script.js
@@ -4,7 +4,7 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-getUser('vbudithi');
+
 
 async function getUser(user){
       const resp = await fetch(APIURL + user);
@@ -29,11 +29,10 @@ function createUserCard(user){
                
                 <p>${user.bio} </p>
           
-                
                 <ul class="info">
-                    <li>Followers ${user.followers} </li>
-                    <li>Following ${user.following} </li>
-                    <li>Github Repos ${user.public_repos}</li>
+                    <li><Strong>Followers</strong> ${user.followers} </li>
+                    <li><strong>Following</strong> ${user.following} </li>
+                    <li><strong>Github Repos</strong> ${user.public_repos}</li>
                 </ul>
                 <p class="icon"> <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
                 </p>
