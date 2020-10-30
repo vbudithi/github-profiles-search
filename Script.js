f49aa76bfc1d36736745448682ab65787a60b115
vbudithi <vivek.budithi@gmail.com>
2020-10-31 02:43:14 +0930

    repos created

--- Script.js
+++ Script.js
@@ -4,22 +4,27 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
+getUser("vbudithi");
 
-
-async function getUser(user){
-      const resp = await fetch(APIURL + user);
+async function getUser(username){
+      const resp = await fetch(APIURL + username);
       const respData = await resp.json();
 
       createUserCard(respData);
+
+      getRepos(username);
 }
 
-function createUserCard(user){
-    const card = document.createElement("div");
-    card.classList.add("card");
+ async function getRepos(username){
+     const resp = await fetch(APIURL + username + "/repos");
+     const respData = await resp.json();
 
+     addReposToCard(respData);
+ }
+
+function createUserCard(user){
    const cardHTML = `
-        <div class="card">  
-           
+        <div class="card">          
             <div>
                 <img class ="avatar" src ="${user.avatar_url}" alt="${user.name}" />
             </div>
@@ -37,11 +42,33 @@ function createUserCard(user){
                 <p class="icon"> <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
                 </p>
                   <p>(<i>${user.location})</i></p>
+
+                <h4>Repos:</h4>
+                <div id="repos"></div>
             </div>
         </div>
     `;
 
     main.innerHTML = cardHTML; 
+}
+
+function addReposToCard(repos){
+    const reposE1 = document.getElementById
+    ("repos");
+
+    repos
+        .sort((a,b) => a.stargazers_count - b.stargazers_count)
+        .slice(0,10)
+        .forEach(repo =>{
+            const repoE1 = document.createElement('a');
+            repoE1.classList.add('repo');
+
+            repoE1.href = repo.html_url;
+            repoE1.target="_blank";
+            repoE1.innerText = repo.name;
+
+            reposE1.appendChild(repoE1);
+    });
 }
 
 form.addEventListener('submit', (e) => {
