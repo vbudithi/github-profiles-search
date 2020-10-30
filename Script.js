a10023eab3f51116934ed5f176c22360bd4ba577
vbudithi <vivek.budithi@gmail.com>
2020-10-30 23:28:10 +0930

    extracting profiles

--- Script.js
+++ Script.js
@@ -1,31 +1,48 @@
-const APIURL = 'https://api.github.com/users/'
+const APIURL = 'https://api.github.com/users/';
 
-async function getUSer(user){
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+async function getUser(user){
       const resp = await fetch(APIURL + user);
       const respData = await resp.json();
-
 
       createUserCard(respData);
 }
 
 function createUserCard(user){
-    const card = document.createElement(div);
-    card.classList.add('card');
+    const card = document.createElement("div");
+    card.classList.add("card");
 
-    card.innerHTML = `
-        <div>
-           <img src ="${user.avatar_url}" alt="${user.name}" />
+   const cardHTML = `
+        <div class="card">     
+            <div>
+                <img src ="${user.avatar_url}" alt="${user.name}" />
+            </div>
+            <div>
+                <h2>${user.name}</h2>
+                <p>${user.bio} (Location: <i>${user.location})</i></p>
+                    
+                <ul>
+                    <li>${user.followers} </li>
+                    <li>${user.following} </li>
+                    <li>${user.public_repos}</li>
+                </ul>
+            </div>
         </div>
-        <div>
-        <h2>${user.name}</h2>
-        <p>${user.bio}</p>
-        
-        <ul>
-         <li>${user.followers} </li>
-         <li>${user.following} <li>
-         <li>${user.public_repos}</li>
-        </ul>
-        </div>
-
     `;
+
+    main.innerHTML = cardHTML; 
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const user = search.value;
+
+    if (user) {
+        getUser(user);
+        search.value = " ";
+    }
+});
